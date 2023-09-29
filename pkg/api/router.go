a6ff484a054a429e951963d281034043ef518fcb
Avyukth <makur.subhrajit@gmail.com>
2023-09-29 22:50:44 +0530

    adding router

---
+++ pkg/api/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/avyukth/search-app/pkg/api/handler"
+	"github.com/avyukth/search-app/pkg/database/mongo"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+// SetupRoutes sets up all the routes for your application
+func SetupRoutes(app *fiber.App, db *mongo.Database) {
+	// Middleware
+	app.Use(logger.New()) // add logging to each request
+
+	// Routes
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	// Define your routes here
+	v1.Get("/search", handler.SearchHandler(db))
+	v1.Get("/download", handler.DownloadHandler(db))
+}
