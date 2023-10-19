c97de7cab462f5de821f5ee9d532ef46129475c5
Avyukth <makur.subhrajit@gmail.com>
2023-10-19 16:13:21 +0530

    fixing server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,112 +1,5 @@
 package main
 
-// import (
-// 	"context"
-// 	"errors"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"os/signal"
-// 	"syscall"
-// 	"time"
-
-// 	_ "github.com/avyukth/search-app/docs"
-// 	appTrace "github.com/avyukth/search-app/foundations/tracing"
-// 	"github.com/avyukth/search-app/pkg/api/router"
-// 	"github.com/avyukth/search-app/pkg/config"
-// 	"github.com/avyukth/search-app/pkg/database/mongo"
-// 	"github.com/avyukth/search-app/pkg/downloader"
-// 	"github.com/avyukth/search-app/pkg/indexer"
-// 	"github.com/avyukth/search-app/pkg/parser"
-// 	"github.com/avyukth/search-app/pkg/queue"
-// 	"github.com/avyukth/search-app/pkg/worker"
-// 	"github.com/gofiber/fiber/v2"
-// 	"github.com/gofiber/fiber/v2/middleware/cors"
-// 	"github.com/gofiber/swagger"
-// 	"github.com/joho/godotenv"
-// )
-
-// func main() {
-// 	// Load .env file from current directory
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("Error loading .env file")
-// 	}
-
-// 	// Load Configuration
-// 	cfg, err := config.LoadConfig()
-// 	if err != nil {
-// 		log.Fatalf("Error loading configurations: %v", err)
-// 	}
-
-// 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-// 	defer stop()
-
-// 	otelShutdown, err := appTrace.SetupOTelSDK(ctx, cfg.ServerConfig.ServiceName, cfg.ServerConfig.ServiceVersion)
-
-// 	defer func() {
-// 		err = errors.Join(err, otelShutdown(context.Background()))
-// 	}()
-
-// 	// Setup Database Connection
-// 	db, err := mongo.SetupDatabase(cfg)
-// 	if err != nil {
-// 		log.Fatalf("Error setting up database: %v", err)
-// 	}
-// 	defer db.Client.Disconnect(context.TODO())
-
-// 	// Initialize components
-// 	httpClient := &http.Client{Timeout: 30 * time.Second,}
-// 	parser := parser.NewParser()
-
-// 	indexer, err := indexer.NewSearchEngine(cfg.ServerConfig.Storage + cfg.ServerConfig.IndexDirectory)
-// 	if err != nil {
-// 		log.Fatalf("Error loading configurations: %v", err)
-// 	}
-
-// 	dl := downloader.NewDownloader(httpClient, &cfg.ServerConfig)
-// 	wk := worker.NewWorker(dl, parser, db, indexer)
-// 	q := queue.NewTaskQueue(10, wk)
-// 	swaggerURL := fmt.Sprintf("http://127.0.0.1:%d/docs/swagger.yaml", cfg.ServerConfig.Port)
-
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
-// 	q.Start(ctx)
-
-// 	//testing
-// 	// Setup Fiber App
-// 	app := fiber.New(fiber.Config{
-// 		Prefork:       false,
-// 		CaseSensitive: true,
-// 		StrictRouting: true,
-// 	})
-
-// 	app.Static("/docs", "./docs")
-// 	app.Use(cors.New())
-
-// 	app.Get("/swagger/*", swagger.New(swagger.Config{
-// 		URL: swaggerURL,
-// 	}))
-
-// 	// Setup Router
-// 	router.SetupRoutes(app, db, indexer, q)
-
-// 	// Start Server
-// 	go func() {
-// 		log.Printf("Starting Server on port %d", cfg.ServerConfig.Port)
-// 		if err := app.Listen(fmt.Sprintf(":%d", cfg.ServerConfig.Port)); err != nil {
-// 			log.Fatalf("Error starting the app: %v", err)
-// 		}
-// 	}()
-
-// 	// Graceful Shutdown
-// 	c := make(chan os.Signal, 1)
-// 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-// 	_ = <-c
-// 	log.Println("Graceful Shutdown...")
-// 	_ = app.Shutdown()
-// }
-
 import (
 	"context"
 	// "sync"
@@ -137,87 +30,7 @@ import (
 )
 
 
-// func main() {
-// 	// Load Configuration
-// 	cfg, err := config.LoadConfig()
-// 	if err != nil {
-// 		log.Fatalf("Error loading configurations: %v", err)
-// 	}
 
-
-// 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-// 	defer stop()
-
-// 	otelShutdown, err := appTrace.SetupOTelSDK(ctx, cfg.ServerConfig.ServiceName, cfg.ServerConfig.ServiceVersion)
-
-// 	defer func() {
-// 		err = errors.Join(err, otelShutdown(context.Background()))
-// 	}()
-
-// 	// Setup Database Connection
-// 	db, err := mongo.SetupDatabase(cfg)
-// 	if err != nil {
-// 		log.Fatalf("Error setting up database: %v", err)
-// 	}
-// 	defer db.Client.Disconnect(context.TODO())
-
-// 	// Initialize components
-// 	httpClient := &http.Client{Timeout: 30 * time.Second,}
-// 	parser := parser.NewParser()
-
-// 	indexer, err := indexer.NewSearchEngine(cfg.ServerConfig.Storage + cfg.ServerConfig.IndexDirectory)
-// 	if err != nil {
-// 		log.Fatalf("Error loading configurations: %v", err)
-// 	}
-
-// 	dl := downloader.NewDownloader(httpClient, &cfg.ServerConfig)
-// 	wk := worker.NewWorker(dl, parser, db, indexer)
-// 	q := queue.NewTaskQueue(10, wk)
-// 	swaggerURL := fmt.Sprintf("/docs/swagger.yaml")
-
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
-// 	q.Start(ctx)
-
-// 	//testing
-// 	// Setup Fiber App
-// 	app := fiber.New(fiber.Config{
-// 		Prefork:       false,
-// 		CaseSensitive: true,
-// 		StrictRouting: true,
-// 	})
-
-// 	app.Static("/docs", "./docs")
-// 	app.Use(cors.New())
-
-// 	app.Get("/swagger/*", swagger.New(swagger.Config{
-// 		URL: swaggerURL,
-// 	}))
-
-// 	// Setup Router
-// 	router.SetupRoutes(app, db, indexer, q)
-
-
-// 	appConfig :=  AppConfig{
-// 		ListenAddr: fmt.Sprintf(":%d", cfg.ServerConfig.ServicePort),
-// 		ServiceName: cfg.ServerConfig.ServiceName,
-// 		ServiceVersion: cfg.ServerConfig.ServiceVersion,
-// 		FiberConfig: fiber.Config{
-// 			Prefork:               false,
-// 			ServerHeader:          "Fiber",
-// 			ReadTimeout:           time.Second,
-// 			WriteTimeout:          10 * time.Second,
-// 			DisableStartupMessage: false,
-// 		},
-// 	}
-
-
-// 	go startApp(app, appConfig.ListenAddr)
-
-// 	// Wait for interrupt signal to gracefully shut down the server.
-// 	waitForShutdownSignal(app)
-
-// }
 func main() {
 	// var wg sync.WaitGroup
 	if err := godotenv.Load(); err != nil {
@@ -254,7 +67,6 @@ func loadConfig() *config.Config {
 
 func setupContext() (context.Context, context.CancelFunc) {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	
 	return ctx, stop
 }
 
