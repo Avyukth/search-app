af21949ab4641532789c59bfa97fb6bdf54e78f6
Avyukth <makur.subhrajit@gmail.com>
2023-10-19 19:13:54 +0530

    adding viper

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.49.2
 	github.com/gofiber/swagger v0.1.13
 	github.com/joho/godotenv v1.5.1
+	github.com/spf13/viper v1.17.0
 	github.com/swaggo/swag v1.16.2
 	go.mongodb.org/mongo-driver v1.12.1
 	go.opentelemetry.io/otel v1.19.0
@@ -42,6 +43,7 @@ require (
 	github.com/blevesearch/zapx/v15 v15.3.13 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
@@ -53,23 +55,34 @@ require (
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/mschoch/smat v0.2.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/prometheus/client_golang v1.16.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.10.0 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
@@ -83,15 +96,20 @@ require (
 	go.opentelemetry.io/otel/metric v1.19.0 // indirect
 	go.opentelemetry.io/otel/trace v1.19.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	golang.org/x/tools v0.7.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230711160842-782d3b101e98 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
 	google.golang.org/grpc v1.58.2 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
 type MongoDBConfig struct {
@@ -49,37 +51,70 @@ type Config struct {
 	ServerConfig
 }
 
-// LoadConfig loads configuration from environment variables.
 func LoadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	// Define the default values
+
+	viper.SetDefault("MONGODB_USERNAME", "root")
+	viper.SetDefault("MONGODB_PASSWORD", "example")
+	viper.SetDefault("MONGODB_DATABASE", "searchDB")
+	viper.SetDefault("MONGODB_MAX_POOL_SIZE", 10)
+	viper.SetDefault("MONGODB_STORAGE_COLLECTION_NAME", "patent")
+	viper.SetDefault("MONGODB_INDEX_COLLECTION_NAME", "indexPatent")
+	viper.SetDefault("MONGODB_LINK_COLLECTION_NAME", "downloadLink")
+	viper.SetDefault("REDIS_PASSWORD", "yourpassword")
+	viper.SetDefault("REDIS_ADDRESS", "localhost:6379")
+	viper.SetDefault("REDIS_DB", 0)
+	viper.SetDefault("SERVER_PORT", 40051)
+	viper.SetDefault("SERVER_HOST", "localhost")
+	viper.SetDefault("REDIS_TIMEOUT", 10)
+	viper.SetDefault("SERVER", "localhost")
+	viper.SetDefault("MONGO_CONTAINER_NAME", "mongodb-search")
+	viper.SetDefault("REDIS_CONTAINER_NAME", "redis-search")
+	viper.SetDefault("INDEX_DIRECTORY", "/index")
+	viper.SetDefault("DATA_STORE_DIRECTORY", "./search-data")
+	viper.SetDefault("STORAGE_DIRECTORY", "./storage")
+	viper.SetDefault("VERSION", "1.0")
+
+	// Read the configuration from environment variables
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+		return nil, err
+	}
+
 	return &Config{
 		MongoDBConfig: MongoDBConfig{
-			Host:                  getEnv("MONGODB_HOST", "localhost"),
-			Port:                  getEnvAsInt("MONGODB_PORT", 27017),
-			Username:              getEnv("MONGODB_USERNAME", "root"),
-			Password:              getEnv("MONGODB_PASSWORD", "example"),
-			URI:                   getEnv("MONGODB_URI", "mongodb://%s:%s@localhost:27017"),
-			Database:              getEnv("MONGODB_DATABASE", "searchDB"),
-			MaxPoolSize:           getEnvAsUInt("MONGODB_MAX_POOL_SIZE", 10),
-			ContainerName:         getEnv("MONGO_CONTAINER_NAME", "mongodb-search"),
-			StorageCollectionName: getEnv("MONGODB_STORAGE_COLLECTION_NAME", "patent"),
-			IndexCollectionName:   getEnv("MONGODB_INDEX_COLLECTION_NAME", "indexPatent"),
-			LinkCollectionName:    getEnv("MONGODB_LINK_COLLECTION_NAME", "downloadLink"),
+			Host:                  viper.GetString("MONGODB_HOST"),
+			Port:                  viper.GetInt("MONGODB_PORT"),
+			Username:              viper.GetString("MONGODB_USERNAME"),
+			Password:              viper.GetString("MONGODB_PASSWORD"),
+			Database:              viper.GetString("MONGODB_DATABASE"),
+			MaxPoolSize:           viper.GetUint64("MONGODB_MAX_POOL_SIZE"),
+			URI:                   viper.GetString("MONGODB_URI"),
+			ContainerName:         viper.GetString("MONGO_CONTAINER_NAME"),
+			StorageCollectionName: viper.GetString("MONGODB_STORAGE_COLLECTION_NAME"),
+			IndexCollectionName:   viper.GetString("MONGODB_INDEX_COLLECTION_NAME"),
+			LinkCollectionName:    viper.GetString("MONGODB_LINK_COLLECTION_NAME"),
 		},
 		RedisConfig: RedisConfig{
-			Password:      getEnv("REDIS_PASSWORD", "yourpassword"),
-			Address:       getEnv("REDIS_ADDRESS", "localhost:6379"),
-			DB:            getEnvAsInt("REDIS_DB", 0),
-			Timeout:       time.Duration(getEnvAsInt("REDIS_TIMEOUT", 10)) * time.Second,
-			ContainerName: getEnv("REDIS_CONTAINER_NAME", "redis-search"),
+			Password:      viper.GetString("REDIS_PASSWORD"),
+			Address:       viper.GetString("REDIS_ADDRESS"),
+			DB:            viper.GetInt("REDIS_DB"),
+			Timeout:       time.Duration(viper.GetInt("REDIS_TIMEOUT")) * time.Second,
+			ContainerName: viper.GetString("REDIS_CONTAINER_NAME"),
 		},
 		ServerConfig: ServerConfig{
-			ServicePort:        getEnvAsInt("SERVER_PORT", 40051),
-			Server:             getEnv("SERVER_HOST", "localhost"),
-			IndexDirectory:     getEnv("INDEX_DIRECTORY", "index"),
-			DataStoreDirectory: getEnv("DATA_STORE_DIRECTORY", "search-data"),
-			Storage:            getEnv("STORAGE_DIRECTORY", "storage"),
-			ServiceName:        getEnv("SERVICE_NAME", "search-app"),
-			ServiceVersion:     getEnv("SERVICE_VERSION", "1.0.0"),
+			ServiceHost:        viper.GetString("SERVER_HOST"),
+			ServicePort:        viper.GetInt("SERVER_PORT"),
+			Server:             viper.GetString("SERVER"),
+			IndexDirectory:     viper.GetString("INDEX_DIRECTORY"),
+			DataStoreDirectory: viper.GetString("DATA_STORE_DIRECTORY"),
+			Storage:            viper.GetString("STORAGE_DIRECTORY"),
+			ServiceName:        viper.GetString("SERVICE_NAME"),
+			ServiceVersion:     viper.GetString("VERSION"),
 		},
 	}, nil
 }
